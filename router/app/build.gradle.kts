c60c0ca03d782edec7800b110724ff204dc0d449
harisson.nas@gmail.com <harisson.nas@gmail.com>
2022-06-05 21:16:35 -0300

    add avro support

--- router/app/build.gradle.kts
+++ router/app/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.github.davidmc24.gradle.plugin.avro.GenerateAvroJavaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("org.springframework.boot") version "2.7.1-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	id ("com.github.davidmc24.gradle.plugin.avro") version "1.3.0"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
@@ -22,12 +24,32 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.apache.avro:avro:1.11.0")
+	implementation("io.confluent:kafka-avro-serializer:7.1.1")
+	implementation("org.apache.kafka:kafka_2.13:3.2.0")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 }
 
+val avroGen by tasks.register("generateAvroJavaClasses", GenerateAvroJavaTask::class.java) {
+	setSource("src/main/resources/avro")
+	setOutputDir(file("src/main/java"))
+}
+
+avro{
+	setOutputCharacterEncoding("UTF-8")
+	setStringType("CharSequence")
+}
+
+tasks.withType<JavaCompile> {
+	source(avroGen)
+}
+
 tasks.withType<KotlinCompile> {
+	dependsOn(avroGen)
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "11"
